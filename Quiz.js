bdf785285ebc71d768e91a6d8d466aa5299adf94
Hassanah-Nasiru <hassananasiru2004@gmail.com>
2024-09-30 11:06:39 -0100

    Update Quiz.js
     Function to start the quiz

--- Quiz.js
+++ Quiz.js
@@ -41,4 +41,19 @@ function startGame() {
     resetState()
     showQuestion(shuffledQuestions[currentQuestionIndex])
   }
+// Function to start the quiz
+function startGame() {
+    score = 0 // Reset score at the start of each game
+    startButton.classList.add('hide')
+    shuffledQuestions = questions.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0
+    questionContainerElement.classList.remove('hide')
+    setNextQuestion()
+  }
   
+  // Function to set the next question
+  function setNextQuestion() {
+    resetState()
+    showQuestion(shuffledQuestions[currentQuestionIndex])
+  }
+    
